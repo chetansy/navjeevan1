ecf8f82a9cc7251e0467f90c762153f27900bc18
chetansy <62282151+chetansy@users.noreply.github.com>
2023-09-21 11:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ app.config["SECRET_KEY"] = 'root'
 app.config["MAIL_SERVER"]='smtp.googlemail.com'
 app.config["MAIL_USE_TLS"] = True      
 app.config["MAIL_PORT"] = 587      
-app.config["MAIL_USERNAME"] = 'chetan0yewale@gmail.com'  
-app.config['MAIL_PASSWORD'] = 'kqxd ixvw jjfc tgrx'  
+app.config["MAIL_USERNAME"] = 'navjeevan.creditsiddhi@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'endf ghdw unlj ajtc'  
 
 
 mail = Mail(app)
