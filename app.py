5abb9bd6b9c9567877035a2d721d900fff26cd41
chetansy <62282151+chetansy@users.noreply.github.com>
2023-10-04 11:51:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def get_neo_score(email):
 	
 	# Loading later
 	with open('neo_score_model_and_transformers3.pkl', 'rb') as f:
-		saved_objects = pickle.load(f, encoding='latin1')
+		saved_objects = pickle.load(f)
 	#saved_objects = pickle.load(open('neo_score_model_and_transformers2.pkl', 'rb'))
 	#saved_objects = joblib.load('neo_score_model_and_transformers2.sav')
 	model = saved_objects['model']
